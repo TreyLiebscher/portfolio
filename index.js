7a85b2e15bee84aa969374bdb73a88b010e32b8d
treyliebscher <wcliebsc@gmail.com>
2018-12-13 11:42:40 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+'use strict';
+
+function handleContactClick(){
+  $('#contactButton').on('click', (ev) => {
+    ev.preventDefault();
+    
+    $('html, body').animate({scrollTop:$(document).height()}, 'slow');
+    
+    const glowAnimation = new Promise((res, rej) => {
+      setTimeout(() => {
+      res($('.contactBox').toggleClass('glow'));
+      }, 200);
+      setTimeout(() => {
+        res($('.contactBox').toggleClass('glow'));
+      }, 1000);
+    });
+    
+    const contactGlow = new Promise((res, rej) => {
+      setTimeout(() => {
+        res($('.contact').toggleClass('glow'));
+      }, 800);
+      setTimeout(() => {
+        res($('.contact').toggleClass('glow'));
+      }, 1600);
+    });
+    
+  });
+}
+
+function renderProjectBox(pro){
+  return `
+          <div class="projectBox faded" id="project-box">
+            <h2 class="projectTitle">${pro.title}</h2>
+            <div class="linkBox">
+              <a href=${pro.repo} target="blank" class="projectLink">Code</a>
+              <a href="${pro.demo}" target="blank" class="projectLink">Live Demo</a>
+            </div>
+            <img src="${pro.screenshot}" class="screenshot" />
+            <p class="projectText">
+              ${pro.description}
+            </p>
+            <div class="techIcons">
+              ${pro.icons.join("")}
+            </div>
+            <button id="info-button" class="infoButton">MORE INFO</button>
+          </div>
+  `
+}
+
+function handleProjectsClick(){
+  $('#projectButton').on('click', (ev) => {
+    ev.preventDefault();
+    const renderedProjects = projects.map((project) => {
+      return renderProjectBox(project);
+    });
+    $('#main-content').html(renderedProjects);
+    
+    const projectFadeIn = new Promise((res, rej) => {
+      setTimeout(() => {
+        res($('.projectBox').toggleClass('faded'));
+      }, 250);
+    });
+    
+  })
+}
+
+function handleAboutClick(){
+  $('#aboutButton').on('click', (ev) => {
+    ev.preventDefault();
+    $('#main-content').html(aboutMe)
+  });
+}
+
+function portfolio(){
+  $(handleContactClick);
+  $(handleAboutClick);
+  $(handleProjectsClick);  
+}
+
+$(portfolio);
